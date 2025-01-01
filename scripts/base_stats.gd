741b4ae13409d8c7427853a6b36b9f1e0d676e4c
Harvey Limbo <limboh27@gmail.com>
2024-12-31 19:04:11 -0800

    add 1d graph and have node move along curve

---
+++ scripts/base_stats.gd
@@ -0,0 +1,12 @@
+extends RefCounted
+class_name BaseStats
+
+var name: String
+var level: int
+var exp_points: int
+var skill_points: int
+
+var hp: int
+var attack: int
+var defense: int
+var speed: int
---
+++ scripts/current_stats.gd
@@ -0,0 +1,8 @@
+extends RefCounted
+class_name current_stats
+
+var hp: int
+var skill_points: int
+var attack: int
+var defense: int
+var speed: int

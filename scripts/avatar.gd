5a223020f87099ce42177a8b27c5aa263806015d
Harvey Limbo <limboh27@gmail.com>
2024-12-31 22:20:47 -0800

    connect attack, defend, skill usage, and fleeing to 1d graph timeline

---
+++ scripts/avatar.gd
@@ -0,0 +1,25 @@
+extends PathFollow2D
+class_name Avatar
+
+@export var move_speed: float = 5
+var _curr_speed: float
+
+@onready var path_follow_2d: PathFollow2D = $"."
+@onready var area_2d: Area2D = $Sprite2D/Area2D
+
+signal on_start_order_step(body: Node2D)
+signal on_start_exe_step(body: Node2D)
+
+func _ready() -> void:
+	_curr_speed = move_speed
+	area_2d.body_entered.connect(on_area_entered)
+	area_2d.body_exited.connect(on_area_exited)
+
+func _physics_process(delta: float) -> void:
+	path_follow_2d.progress_ratio += delta * _curr_speed
+
+func on_area_entered(body: Node2D) -> void:
+	on_start_order_step.emit(body)
+
+func on_area_exited(body: Node2D) -> void:
+	on_start_exe_step.emit(body)
--- scripts/ui_controller.gd
+++ scripts/ui_controller.gd
@@ -29,6 +29,14 @@ const ON_TARGET_HOVERED: String = "on_target_hovered"
 const ON_TARGET_UNHOVERED: String = "on_target_unhovered"
 const ON_TARGET_CLICKED: String = "on_target_clicked"
 
+## 1-D Graph variables
+@onready var one_d_graph: Control = $OneDGraph
+@onready var avatar: Avatar = one_d_graph.get_node("PartyPath2D/Avatar")
+const ORDER_STEP: float = 0.858
+var did_avatar_skill_start: bool = false
+
+@onready var skill_timer: Timer = $SkillTimer
+
 func _ready():
 	attack.pressed.connect(_on_attack_button_pressed)
 	defend.pressed.connect(_on_defend_button_pressed)
@@ -53,6 +61,10 @@ func _ready():
 	mob2.connect(ON_TARGET_CLICKED, on_target_clicked)
 	mob3.connect(ON_TARGET_CLICKED, on_target_clicked)
 	
+	# 1-d graph
+	avatar.on_start_order_step.connect(on_start_order_step)
+	avatar.on_start_exe_step.connect(on_start_exe_step)
+	
 func _on_attack_button_pressed():
 	action_layout.visible = false
 	
@@ -73,6 +85,7 @@ func _on_defend_button_pressed():
 	# TODOS
 	# increase player's defense temporarily for a set time
 	# set it back to what it was previously
+	avatar.progress_ratio = 1
 	description_panel.visible = true
 	action_layout.visible = false
 	var player_name := "Mumbo"
@@ -96,20 +109,37 @@ func _on_description_timer_timeout():
 	description_panel.visible = false
 	label.text = ""
 	
-	action_layout.visible = true
-	
+	# once party member moves up to do their attack or complete their ability
+	# complete means end of animation and attack damage is dealt
+	# reset the avatar that made the move back to the beginning of timeline
+	avatar.progress_ratio = 0
+	avatar._curr_speed = avatar.move_speed
+
 func _on_skill_activated():
 	# TODOs
 	# check if enough SRP to cast skill
 	# possibly move to SkillContainer and create a skill container script
-	description_panel.visible = true
 	active_skill_menu.visible = false
+	did_avatar_skill_start = true
 	
+	skill_timer.start()
+	
+
+func _physics_process(delta: float) -> void:
+	if did_avatar_skill_start:
+		# delay skill until avatar progress ratio reaches 1
+		var diff: float = 1 - ORDER_STEP
+		var d: float = diff / skill_timer.wait_time
+		avatar.progress_ratio += d * delta
+
+
+func _on_skill_timer_timeout() -> void:
+	did_avatar_skill_start = false
+	description_panel.visible = true
 	var player_name := "Mumbo"
 	var enemy_name := "Villainous Gelatin"
 	var skill_name := active_placeholder_skill.text
 	var dmg := randi_range(32, 64)
-	
 	label.text = "%s casts %s to %s. It deals %d damage!" % [player_name, skill_name, enemy_name, dmg]
 	description_timer.start()
 
@@ -122,11 +152,13 @@ func _on_flee_button_pressed():
 	var flee_rate := 0.5
 	var roll = randf()
 	description_panel.visible = true
+	action_layout.visible = false
 	if roll < flee_rate:
 		label.text = "Mumbo Party fled!"
 	else:
 		label.text = "Mumbo Party cannot escape!"
-		
+	
+	avatar.progress_ratio = 1
 	description_timer.start()
 
 func on_target_hovered(mob_name: String):
@@ -141,6 +173,9 @@ func on_target_clicked(mob_name: String):
 	mob2.input_pickable = false
 	mob3.input_pickable = false
 	
+	# basic attack - move avatar immediately towards end of exe
+	avatar.progress_ratio = 1
+	
 	target_menu.visible = false
 	description_panel.visible = true
 	var player_name := "Mumbo"
@@ -148,3 +183,21 @@ func on_target_clicked(mob_name: String):
 	label.text = "%s attacked for %d damage to %s" % [player_name, dmg, mob_name]
 	# start timer to hide description panel
 	description_timer.start()
+
+
+func on_start_order_step(body: Node) -> void:
+	print("entering order step")
+	avatar._curr_speed = 0
+	avatar.progress_ratio = ORDER_STEP
+	
+	# pause every avatar motion
+	
+	# entry point for enemies to pick a move
+	
+	# entry point for party member to pick a move
+	action_layout.visible = true
+
+func on_start_exe_step(body: Node) -> void:
+	print("entering exe step")
+	# where an action can get emitted depending on which one the player picked
+	# for enemies, this is their entry point to do their action

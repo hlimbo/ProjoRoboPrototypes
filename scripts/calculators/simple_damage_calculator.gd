41422433a1469444e98c65b59eca1796e1bc9e13
Harvey Limbo <limboh27@gmail.com>
2025-01-29 18:03:03 -0800

    fix typo on SimpleDamageCalculator -- it should be damage_receiver's defense and not damage_dealer's defense

--- scripts/calculators/simple_damage_calculator.gd
+++ scripts/calculators/simple_damage_calculator.gd
@@ -3,6 +3,6 @@ class_name SimpleDamageCalculator
 
 func calculate_damage(damage_receiver: Actor, damage_dealer: Actor) -> int:
 	var dd_attack = damage_dealer.avatar.curr_stats.attack
-	var dr_def = damage_dealer.avatar.curr_stats.defense
+	var dr_def = damage_receiver.avatar.curr_stats.defense
 	var dmg = maxi(dd_attack - dr_def, 0)
 	return dmg

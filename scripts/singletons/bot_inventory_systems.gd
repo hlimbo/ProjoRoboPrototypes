f7189d5bf5d33c212a42d362ecb8fc4274aae9ee
Harvey Limbo <limboh27@gmail.com>
2025-02-26 19:21:49 -0800

    WIP move and add operations

--- scripts/singletons/bot_inventory_systems.gd
+++ scripts/singletons/bot_inventory_systems.gd
@@ -1,5 +1,10 @@
 extends Node
 
+enum ContainerType {
+	DIGITAL_BANK,
+	PARTY_BANK,
+}
+
 var digital_bot_bank: BotDataContainer
 var party_bot_bank: BotDataContainer
 
@@ -7,12 +12,12 @@ func _ready():
 	digital_bot_bank = BotDataContainer.new()
 	party_bot_bank = BotDataContainer.new()
 	
-func transfer_bot(bot_name1: String, bot_name2: String):
+func swap_bots(bot_name1: String, bot_name2: String):
 	var b1: AvatarData = digital_bot_bank.get_bot(bot_name1)
 	var b2: AvatarData = party_bot_bank.get_bot(bot_name2)
 	
 	if b1 != null and b2 != null:
-		_transfer_internal(b1, b2)
+		_swap_internal(b1, b2)
 		return
 	
 	# if either b1 or b2 don't exist in the banks above, check the reverse
@@ -20,11 +25,28 @@ func transfer_bot(bot_name1: String, bot_name2: String):
 	b2 = digital_bot_bank.get_bot(bot_name2)
 	
 	assert(b1 != null and b2 != null)
-	_transfer_internal(b1, b2)
+	_swap_internal(b1, b2)
 	
-func _transfer_internal(bot1: AvatarData, bot2: AvatarData):
+func _swap_internal(bot1: AvatarData, bot2: AvatarData):
 	digital_bot_bank.remove_bot(bot1.avatar_name)
 	party_bot_bank.remove_bot(bot2.avatar_name)
 	
 	digital_bot_bank.add_bot(bot2)
 	party_bot_bank.add_bot(bot1)
+
+
+func move_bot(container_src: ContainerType, container_dst: ContainerType, bot_name: String):
+	if container_src == ContainerType.PARTY_BANK and container_dst == ContainerType.DIGITAL_BANK:
+		var bot: AvatarData = party_bot_bank.get_bot(bot_name)
+		party_bot_bank.remove_bot(bot_name)
+		digital_bot_bank.add_bot(bot)
+	elif container_src == ContainerType.DIGITAL_BANK and container_dst == ContainerType.PARTY_BANK:
+		var bot: AvatarData = digital_bot_bank.get_bot(bot_name)
+		digital_bot_bank.remove_bot(bot_name)
+		party_bot_bank.add_bot(bot)
+	# TODO(functional): swap between non-empty slot and empty slot
+	elif container_src == ContainerType.PARTY_BANK and container_dst == ContainerType.PARTY_BANK:
+		pass
+	# TODO(functional): swap between non-empty slot and empty slot
+	elif container_src == ContainerType.DIGITAL_BANK and container_dst == ContainerType.DIGITAL_BANK:
+		pass
--- scripts/ui/digital_bank_controller.gd
+++ scripts/ui/digital_bank_controller.gd
@@ -1,6 +1,17 @@
 extends Control
 class_name DigitalBankController
 
+enum Action {
+	MOVE,
+	ADD,
+	VIEW,
+	NO_OP,
+}
+
+@export var bot_inventory_systems: BotInventorySystems = BotInventorySystems
+
+@export var curr_action: Action = Action.NO_OP
+
 @onready var actions_popup: ActionPopUpView = $ActionsPopup
 @onready var action_label: Label = $ActionLabel
 @onready var grid_container: BotGridView = $GridSlots/GridContainer
@@ -8,6 +19,7 @@ class_name DigitalBankController
 @onready var bot_info: BotDescriptionView = $BotPreview/BotInfo
 
 var selected_cell: BotCellView = null
+var selected_cell2: BotCellView = null
 
 func _ready():
 	actions_popup.on_add.connect(on_add_popup)
@@ -21,7 +33,33 @@ func on_select(curr_cell: BotCellView):
 	# if same cell, no operations required
 	if selected_cell == curr_cell:
 		return
-		
+	
+	# let's try using groups to determine where the cell originates from
+	# so that I know how to move data between 1 data structure to the next
+	# as well as reflect those changes on the view layer
+	match curr_action:
+		Action.MOVE:
+			# case 1 - swap between 2 bots on the same grid
+			# case 2 - swap between 2 bots on grid and on list layout
+			selected_cell2 = curr_cell
+			
+			# case 2
+			bot_inventory_systems.swap_bots(selected_cell2.bot_name, selected_cell.bot_name)
+			
+		Action.ADD:
+			# case 1 - add bot from list layout to grid layout
+			# case 2 - add bot from grid layout to list layout
+			
+			# case 2
+			selected_cell2 = curr_cell
+			bot_inventory_systems.swap_bots(selected_cell.bot_name, selected_cell2.bot_name)
+			
+			pass
+		Action.NO_OP:
+			on_highlight_cell(curr_cell)
+
+func on_highlight_cell(curr_cell: BotCellView):
+	# unhighlight previous selected cell
 	if is_instance_valid(selected_cell):
 		selected_cell.self_modulate = Color.WHITE
 	
@@ -35,25 +73,22 @@ func on_select(curr_cell: BotCellView):
 		actions_popup.global_position = curr_cell.global_position + offset
 
 func on_move_popup():
-	print("move")
 	action_label.visible = true
 	action_label.text = "Action: move mode"
-		
+	curr_action = Action.MOVE
 	actions_popup.visible = false
 
 func on_add_popup():
-	print("add")
-
 	action_label.visible = true
 	action_label.text = "Action: add mode"
-		
+	curr_action = Action.ADD
 	actions_popup.visible = false
 
 func on_view_popup():
-	print("view")
 	action_label.visible = true
 	action_label.text = "Action: view mode"
 	actions_popup.visible = false
+	curr_action = Action.VIEW
 	
 	if is_instance_valid(selected_cell):
 		bot_info.initialize(selected_cell.bot_name, selected_cell.level, selected_cell.bot_type, selected_cell.energy_type)
@@ -64,3 +99,4 @@ func on_close_popup():
 		selected_cell = null
 		
 	action_label.visible = false
+	curr_action = Action.NO_OP

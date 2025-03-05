40e6b2ed30d0a82fd5cfd0079d0f1f93f6c9954c
Harvey Limbo <limboh27@gmail.com>
2025-03-05 09:48:05 -0800

    add buff/debuff fields to skill resource

--- scripts/resources/skill.gd
+++ scripts/resources/skill.gd
@@ -6,6 +6,12 @@ class_name Skill
 @export var name: String
 @export var description: String
 
+# string tags that are applied to target either as buff or debuff
+@export var target_buffs: Array[String]
+@export var target_debuffs: Array[String]
+
+signal on_skill_start(skill: Skill, source_actor: Actor, target_actors: Array[Actor])
+signal on_skill_end(skill: Skill, source_actor: Actor, target_actors: Array[Actor])
 
 func _init(p_cost = 0, p_attack = 0, p_name = "skill_resource"):
 	cost = p_cost

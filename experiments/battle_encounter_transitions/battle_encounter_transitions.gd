60b26f10d867806615799c9ccaa9487a5a341761
Harvey Limbo <limboh27@gmail.com>
2025-04-04 19:37:34 -0700

    wire up cinematic shader transition to a button

--- experiments/battle_encounter_transitions/battle_encounter_transitions.gd
+++ experiments/battle_encounter_transitions/battle_encounter_transitions.gd
@@ -3,8 +3,11 @@ extends Node
 # singleton
 @export var scene_manager: SceneManager = SceneManager
 
+# dependencies
 @export var animation_player: AnimationPlayer
 @export var another_player: AnimationPlayer
+@export var viewport_image_loader: ViewportImageLoader
+
 @export var can_goto_new_scene: bool = false
 @onready var buttons: Array[Button] = []
 
@@ -33,21 +36,35 @@ func on_swirl_pressed():
 	print("swirl pressed")
 	
 func on_after_image_pressed():
+	assert(viewport_image_loader != null)
+	# don't play the shader if in the middle of being processed
+	if viewport_image_loader.is_shader_playing():
+		return
+		
 	print("after image pressed")
+	viewport_image_loader.visible = true
+	await viewport_image_loader.capture_image()
+	viewport_image_loader.play_shader(3.0)
 	
 func on_reset_pressed():
 	animation_player.stop()
+	viewport_image_loader.reset_shader()
 	
 func on_toggle_new_scene():
 	can_goto_new_scene = !can_goto_new_scene
 	
 	if can_goto_new_scene:
 		print("going to new scene on transition")
-		animation_player.animation_finished.connect(goto_new_scene)
+		animation_player.animation_finished.connect(goto_new_scene_anim)
+		viewport_image_loader.on_shader_playing_finished.connect(goto_new_scene)
 	else:
 		print("staying on same scene on transition")
-		animation_player.animation_finished.disconnect(goto_new_scene)
+		animation_player.animation_finished.disconnect(goto_new_scene_anim)
+		viewport_image_loader.on_shader_playing_finished.disconnect(goto_new_scene)
 		
-func goto_new_scene(anim_name: String):
-	print("animation finished: ", anim_name)
+func goto_new_scene():
 	scene_manager.change_scene("res://experiments/battle_encounter_transitions/battle_encounter_prototype.tscn")
+
+func goto_new_scene_anim(anim_name: String):
+	print("animation finished: ", anim_name)
+	goto_new_scene()
--- experiments/battle_encounter_transitions/viewport_image_loader.gd
+++ experiments/battle_encounter_transitions/viewport_image_loader.gd
@@ -1,17 +1,33 @@
 extends TextureRect
 class_name ViewportImageLoader
 
-#6 apply uniform variables exposed from shader to update the shader per from in _process()
+# shader uniform names coming from cinematic_shader.gshader
+const light_percentage = "light_percentage"
+const pixel_offset = "pixel_offset"
+const fade_offset = "fade_offset"
+const main_texture = "main_texture"
+
+var shader_duration: float = 3.0
+var curr_time: float = 0.0
 
 @export var screenshot: Image
 @export var shader: Shader
+@export var screenshot_size: Vector2 = Vector2(1280, 720) # 428, 247
+
+signal on_shader_playing_finished(anim_name: String)
 
 func _ready():
 	self.material = ShaderMaterial.new()
 	load_shader(shader)
+	self.set_process(false)
+
+func is_shader_playing() -> bool:
+	return self.is_processing() and curr_time < shader_duration
 
 # capturing the image works in isolation.... maybe I need to load in the shader AFTER the image is captured?
 func capture_image():
+	# to ensure the frame is visible, processing must be enabled
+	self.set_process(true)
 	# wait til all drawing is complete for the processing frame
 	await RenderingServer.frame_post_draw
 	
@@ -20,7 +36,9 @@ func capture_image():
 	screenshot = get_viewport().get_texture().get_image()
 	assert(screenshot != null)
 	
-	screenshot.resize(300, 300, Image.INTERPOLATE_TRILINEAR)
+	# only resize the image if it's not fullscreen
+	if screenshot_size.x < get_viewport().size.x and screenshot_size.y < get_viewport().size.y:
+		screenshot.resize(screenshot_size.x, screenshot_size.y, Image.INTERPOLATE_TRILINEAR)
 	
 	var image_texture = ImageTexture.create_from_image(screenshot)
 	assert(image_texture != null)
@@ -34,12 +52,51 @@ func load_shader(shader_to_load: Shader):
 	var shader_mat = self.material as ShaderMaterial
 	shader_mat.shader = shader_to_load
 	assert(shader_mat.shader != null)
+
+func play_shader(duration_seconds: float):
+	shader_duration = duration_seconds
+	curr_time = 0.0
 	
-# can be its own script to update the uniform properties of a shader over several frames
-func update_shader_properties():
-	pass
+	self.reset_shader()
+	self.visible = true
+	self.set_process(true)
+	# assign the screenshot taken
+	var shader_mat = self.material as ShaderMaterial
+	shader_mat.set_shader_parameter(main_texture, self.texture)
+	
+func reset_shader():
+	var shader_mat = self.material as ShaderMaterial
+	shader_mat.set_shader_parameter(light_percentage, 0)
+	shader_mat.set_shader_parameter(pixel_offset, 0)
+	shader_mat.set_shader_parameter(fade_offset, 0)
+	self.visible = false
+	self.curr_time = 0.0
 
 func _input(event: InputEvent):
 	if Input.is_action_just_pressed("screenshot"):
 		capture_image()
 		print("capturing image")
+
+func _process(delta: float):
+	if curr_time >= shader_duration:
+		on_shader_playing_finished.emit()
+		self.set_process(false)
+		return
+	
+	var shader_mat = self.material as ShaderMaterial
+	
+	var part1_duration: float = 0.25 * shader_duration
+	var part2_duration: float = 0.5 * shader_duration
+	var percent1: float = curr_time / part1_duration
+	var percent2: float = (curr_time - part1_duration) / part2_duration
+	
+	if curr_time < part1_duration:
+		var light_value: float = lerpf(0.0, 1.0, percent1)
+		var pixel_offset_value: float = lerpf(0.0, 0.005, percent1)
+		shader_mat.set_shader_parameter(light_percentage, light_value)
+		shader_mat.set_shader_parameter(pixel_offset, pixel_offset_value)
+	else:
+		var fade_offset_value: float = lerpf(0.0, 1.0, percent2)
+		shader_mat.set_shader_parameter(fade_offset, fade_offset_value)
+	
+	curr_time += delta

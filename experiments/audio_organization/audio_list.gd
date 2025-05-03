72b7d0996fd274bfd7ab12c4b4b3f5eb282e790c
Harvey Limbo <limboh27@gmail.com>
2025-05-02 17:00:00 -0700

    add audio list script and audio samples for testing

---
+++ experiments/audio_organization/audio_list.gd
@@ -0,0 +1,77 @@
+extends Button
+class_name AudioList
+
+enum Play_Mode {
+	RANDOM,
+	FORWARD, # play stream-players in the order inserted in array
+	BACKWARD, # play stream-players in reverse order
+}
+
+@export var play_mode: Play_Mode = Play_Mode.FORWARD
+@export var audio_player: AudioStreamPlayer
+# can swap out audio streams for 1 player for UI elements
+@export var audio_streams: Array[AudioStream] = []
+
+var play_index: int = 0
+var prev_play_index: int = -1
+var prev_audio_stream: AudioStream = null
+
+func _ready():
+	self.pressed.connect(play)
+
+func play():
+	match play_mode:
+		Play_Mode.FORWARD, Play_Mode.BACKWARD:
+			play_order()
+		Play_Mode.RANDOM:
+			play_random()
+	
+	
+func play_order():
+	if audio_player.playing:
+		return
+	
+	if play_mode == Play_Mode.FORWARD:
+		play_index = (play_index + 1) % len(audio_streams)
+	else:
+		play_index = play_index - 1 if play_index > 0 else len(audio_streams) - 1
+	
+	var stream = audio_streams[play_index]
+	print("playing sound: %s" % stream.resource_path)
+	audio_player.set_stream(stream)
+	audio_player.play()
+
+
+func play_random():
+	if audio_player.playing:
+		return
+		
+	play_index = randi_range(0, len(audio_streams) - 1)
+	var stream: AudioStream = audio_streams[play_index]
+	print("playing random audio: %s" % stream.resource_path)
+	audio_player.set_stream(stream)
+	audio_player.play()
+	
+	# temporarily remove from list to avoid picking same random index on the next play
+	audio_streams.remove_at(play_index)
+	# add previously picked audio stream back to list of items to randomly pick from
+	_add_back_to_list()
+	prev_audio_stream = stream
+	prev_play_index = play_index
+
+func _add_back_to_list():
+	if prev_audio_stream != null and prev_play_index > -1:
+		if prev_play_index >= len(audio_streams):
+			audio_streams.append(prev_audio_stream)
+		else:
+			audio_streams.insert(prev_play_index, prev_audio_stream)
+			
+		var names = audio_streams.map(func(stream): return stream.resource_path)
+		print("NAMES: ", names)
+
+func reset():
+	play_index = 0
+	# move prev player back into the list
+	_add_back_to_list()
+	prev_audio_stream = null
+	prev_play_index = -1
